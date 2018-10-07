118d9f388a461b1b7f5e47d791a35573d0fe9113
Pavel Demin <papyasishe@yandex.ru>
2018-10-08 00:16:20 +0300

    add menu

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
-    $('#search-text').focus(function () {
-        $(this).attr("rel", $(this).attr("placeholder"));
-        $(this).removeAttr("placeholder");
-    });
-    $('#search-text').blur(function () {
-        $(this).attr("placeholder", $(this).attr("rel"));
-        $(this).removeAttr("rel");
-    });
+
+    let search = function() {
+        $('#search-text').focus(function () {
+            $(this).attr("rel", $(this).attr("placeholder"));
+            $(this).removeAttr("placeholder");
+        });
+        $('#search-text').blur(function () {
+            $(this).attr("placeholder", $(this).attr("rel"));
+            $(this).removeAttr("rel");
+        });
+    };
+
+
+    search();
+
+
+
 });
