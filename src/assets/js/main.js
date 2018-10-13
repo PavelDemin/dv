944e15a7868b9c1dce89063dd94122c305971054
Pavel Demin <papyasishe@yandex.ru>
2018-10-14 00:11:56 +0300

    start build content

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -13,13 +13,30 @@ $(document).ready(function(){
 
     let openMenuMobile = function() {
         $('.menu-mobile__icon').click(function() {
-            $('.menu__list-mobile').toggle('fast');
+            $('.menu__list-mobile').addClass('menu__list-mobile-open');
+            $('.menu__close').addClass('menu__close-open');
         });
+
+    };
+
+    let closeMenuMobile = function() {
+        $('.menu__close').click(function() {
+            $('.menu__list').removeClass('menu__list-mobile-open');
+            $(this).removeClass('menu__close-open');
+        });
+    };
+
+    let openMenuTablet = function() {
+      $('.menu__item-tablet').click(function(){
+          $('.menu__item').removeClass('menu__item-mobile-hide');
+      });
     };
 
  
     searchText();
     openMenuMobile();
+    closeMenuMobile();
+    //openMenuTablet();
 
 
 });
